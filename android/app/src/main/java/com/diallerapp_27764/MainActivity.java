5048426141d6457c560fe37d0b1b921e3bce5954
Crowdbotics <team@crowdbotics.com>
2021-06-06 08:22:10 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/diallerapp_27764/MainActivity.java
@@ -0,0 +1,15 @@
+package com.diallerapp_27764;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "diallerapp_27764";
+  }
+}
